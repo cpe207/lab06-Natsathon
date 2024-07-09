25e134f47198eecbfa5c1ee9672261c564026a9c
_pooh <natsathon2018@gmail.com>
2024-07-09 13:29:48 +0700

    commit

--- q2.ts
+++ q2.ts
@@ -3,15 +3,29 @@ import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
+  try {
+    const todoResponse = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    const todo = todoResponse.data;
+
+    const userResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`);
+    const user = userResponse.data;
+
+    return {
+        title: todo.title,
+        completed: todo.completed,
+        owner: user.name,
+    };
+  } catch (error) {
+    return "INVALID TODO ID";
+  }
 };
 
-//test case
+// Test cases
 const input1 = 15;
 const input2 = 60;
 const input3 = 250;
 
-//run
+// Run the tests
 getTodo(input1).then((result) => console.log(result));
 getTodo(input2).then((result) => console.log(result));
 getTodo(input3).then((result) => console.log(result));
